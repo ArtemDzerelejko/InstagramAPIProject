ef729752fa34b9a2e76d9aa2fb040d08d0e382fd
ArtemDzerelejko <artem.dzerelejko@gmail.com>
2023-04-25 22:11:09 +0300

    Initial Commit

---
+++ projectWithInstagram/projectWithInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectWithInstagramApp.swift
+//  projectWithInstagram
+//
+//  Created by artem on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct projectWithInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
